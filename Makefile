97a63297fbea1427f9144d607940100de7baa1d2
Kajetan Bojko <kajboj@googlemail.com>
2017-11-25 13:27:49 +0000

    added PWM

--- Makefile
+++ Makefile
@@ -1,5 +1,9 @@
 # Arduino Make file. Refer to https://github.com/sudar/Arduino-Makefile
 
-ARDUINO_DIR  = ../arduino/arduino-1.8.5
-BOARD_TAG    = uno
+USER_LIB_PATH = ./libraries
+ARDUINO_LIBS  = TimerOne
+ARDUINO_DIR   = ../arduino/arduino-1.8.5
+BOARD_TAG     = uno
+
 include ../arduino/Arduino-Makefile-1.6.0/Arduino.mk
+
--- Omnichord.ino
+++ Omnichord.ino
@@ -1,23 +1,47 @@
+#include "TimerOne.h"
+
+
+int position;
+int width = 1023;
+int step_max = 20;
+int step_min = -step_max;
+
 const int SOFT_POT_PIN = A0;
 const int GRAPH_LENGTH = 60;
 
 void setup() 
 {
-  Serial.begin(9600);
-  pinMode(SOFT_POT_PIN, INPUT);
+  Timer1.initialize();
+  position = random(width);
+
+//  Serial.begin(9600);
+//  pinMode(SOFT_POT_PIN, INPUT);
 }
 
 void loop() 
 {
-  int softPotADC = analogRead(SOFT_POT_PIN);
-  int softPotPosition = map(softPotADC, 0, 1023, 0, GRAPH_LENGTH);
+  Timer1.pwm(9, position, 8);
 
-  Serial.print("<"); // Starting end
-  for (int i=0; i<GRAPH_LENGTH; i++) {
-    if (i == softPotPosition) Serial.print("|");
-    else Serial.print("-");
-  }
-  Serial.println("> (" + String(softPotADC) + ")");
+  position = position + random(step_min, step_max);
+  if (position > width) {
+    position = 2*width - position;
+  } else {
+    if (position < 0) {
+      position = -position;
+    }
+  };
 
-  delay(25);
+
+
+//  int softPotADC = analogRead(SOFT_POT_PIN);
+//  int softPotPosition = map(softPotADC, 0, 1023, 0, GRAPH_LENGTH);
+//
+//  Serial.print("<"); // Starting end
+//  for (int i=0; i<GRAPH_LENGTH; i++) {
+//    if (i == softPotPosition) Serial.print("|");
+//    else Serial.print("-");
+//  }
+//  Serial.println("> (" + String(softPotADC) + ")");
+//
+//  delay(25);
 }
