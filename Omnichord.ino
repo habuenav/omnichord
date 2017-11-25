3432e954a8762ff894c27c6cd0a8648862dc20ca
Kajetan Bojko <kajboj@googlemail.com>
2017-11-25 20:33:11 +0000

    handy note definitions

--- Omnichord.ino
+++ Omnichord.ino
@@ -10,7 +10,6 @@ const int PWM_SILENCE = 511;
 const int SAMPLING_FREQ = 8000; // Hz
 const float SAMPLE_FREQ = 130.81; // Hz (C-3)
 const float SAMPLES_PER_TICK = (float) SAMPLING_FREQ / TIMER_RESOLUTION;
-const int SAMPLE_MULTIPLIER = 4;
 const byte SAMPLE_SILENCE = 0;
 
 const int SOFT_POT_PIN = A0;
@@ -20,16 +19,29 @@ typedef long SampleIndex;
 typedef long Microseconds;
 typedef float Frequency;
 
+float samplesPerTick(float freq) {
+  return freq/SAMPLE_FREQ * SAMPLES_PER_TICK;
+}
+
 typedef struct {
-  Frequency freq;
+  float samplesPerTick;
+} Note;
+
+#import "notes"
+
+typedef struct {
+  Note *note;
   Microseconds triggered;
 } Channel;
 
 Channel channels[] = {
-  { 130.81, 1*TIMER_RESOLUTION },
-  { 164.81, 1.5*TIMER_RESOLUTION },
-  { 261.62, 2*TIMER_RESOLUTION },
+  { C_3, 1*TIMER_RESOLUTION },
+  { E_3, 1.5*TIMER_RESOLUTION },
+  { G_3, 2*TIMER_RESOLUTION },
+  { Bb3, 2.5*TIMER_RESOLUTION },
 };
+
+int numberOfChannels = sizeof(channels)/sizeof(Channel);
 
 void setup() 
 {
@@ -41,9 +53,10 @@ void setup()
 
 char sampled(Microseconds time, int channelIndex) {
   Channel channel = channels[channelIndex];
-  float samplesPerTick = channel.freq/SAMPLE_FREQ * SAMPLES_PER_TICK;
+
   Microseconds currentSampleTime = time - channel.triggered;
-  SampleIndex sampleIndex = currentSampleTime * samplesPerTick;
+  SampleIndex sampleIndex = currentSampleTime * channel.note->samplesPerTick;
+
   if (sampleIndex >= 0 && sampleIndex < SAMPLE_SIZE) {
     return pgm_read_byte_near(SAMPLE + sampleIndex);
   } else {
@@ -53,10 +66,15 @@ char sampled(Microseconds time, int channelIndex) {
 
 void loop() 
 {
- Microseconds t = micros();
- int sum = sampled(t, 0) + sampled(t, 2);
- int output = PWM_SILENCE + sum * SAMPLE_MULTIPLIER/2;
- Timer1.pwm(PWM_PIN, output, PWM_PERIOD);
+  Microseconds t = micros();
+  int sum = 0;
+ 
+  for (int i=0; i<numberOfChannels; i++) {
+    sum += sampled(t, i);
+  }
+ 
+  int output = PWM_SILENCE + sum;
+  Timer1.pwm(PWM_PIN, output, PWM_PERIOD);
 
 //  int softPotADC = analogRead(SOFT_POT_PIN);
 //  int softPotPosition = map(softPotADC, 0, 1023, 0, GRAPH_LENGTH);
